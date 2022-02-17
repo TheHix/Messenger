2ef0989bf82a7c5fc85bb1d98df80d3a27385cb2
Ssaveliy <saveliy-sokolnikov@mail.ru>
2022-02-17 10:03:47 +0300

    added functionality to buttons in the main window

--- js/main.js
+++ js/main.js
@@ -1,9 +1,24 @@
-import {PAGE} from "./view.js";
-PAGE.BTN_SETTINGS.addEventListener('click', () => {
+import {PAGE, URL} from "./view.js";
+PAGE.EXIT_BTN.forEach((e) => {
+    e.addEventListener('click', () => {
+        closeWindows();
+        PAGE.MAIN_WINDOW.classList.remove('blur');
+    });
+})
+PAGE.SETTINGS_BTN.addEventListener('click', () => {
+    closeWindows();
     PAGE.SETTINGS_WINDOW.classList.add('active');
     PAGE.MAIN_WINDOW.classList.add('blur');
 });
-PAGE.EXIT.addEventListener('click', () => {
-    PAGE.SETTINGS_WINDOW.classList.remove('active');
-    PAGE.MAIN_WINDOW.classList.remove('blur');
+PAGE.LOG_BTN.addEventListener('click', () => {
+    closeWindows();
+    PAGE.LOG_WINDOW.classList.add('active');
+    PAGE.MAIN_WINDOW.classList.add('blur');
+    PAGE.EMAIL_FORM.addEventListener('submit', (e) => {
+        e.preventDefault();
+    })
 });
+function closeWindows() {
+    PAGE.SETTINGS_WINDOW.classList.remove('active');
+    PAGE.LOG_WINDOW.classList.remove('active');
+}
--- js/view.js
+++ js/view.js
@@ -1,6 +1,11 @@
 export const PAGE = {
-    BTN_SETTINGS: document.querySelector('.buttons_btnSettings'),
+    SETTINGS_BTN: document.querySelector('.buttons_btnSettings'),
     SETTINGS_WINDOW: document.querySelector('.settings'),
     MAIN_WINDOW:document.querySelector('.main'),
-    EXIT: document.querySelector('.cross')
+    EXIT_BTN: document.querySelectorAll('.cross'),
+    LOG_BTN: document.querySelector('.buttons_logInOut'),
+    LOG_WINDOW: document.querySelector('.LogInOut'),
+    EMAIL_FORM: document.querySelector('.authorizationForm'),
+    INPUT_EMAIL: document.querySelector('.authorizationForm__email')
 };
+export const URL = 'https://chat1-341409.oa.r.appspot.com/api/user';
