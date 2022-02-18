86b5a0768cf47752c7f55c2c10a7f8bbc6ab1044
Ssaveliy <saveliy-sokolnikov@mail.ru>
2022-02-18 20:59:30 +0300

    added username update function

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 import {PAGE, URL} from "./view.js";
+import Cookies from 'js-cookie';
 PAGE.EXIT_BTN.forEach((e) => {
     e.addEventListener('click', () => {
         closeWindows();
@@ -9,6 +10,10 @@ PAGE.SETTINGS_BTN.addEventListener('click', () => {
     closeWindows();
     PAGE.SETTINGS_WINDOW.classList.add('active');
     PAGE.MAIN_WINDOW.classList.add('blur');
+    PAGE.NAME_FORM.addEventListener('submit', (e) => {
+        e.preventDefault();
+        requestUsername(PAGE.NAME_INPUT.value, Cookies.get('token'));
+    });
 });
 PAGE.LOG_BTN.addEventListener('click', () => {
     closeWindows();
@@ -16,9 +21,15 @@ PAGE.LOG_BTN.addEventListener('click', () => {
     PAGE.MAIN_WINDOW.classList.add('blur');
     PAGE.EMAIL_FORM.addEventListener('submit', (e) => {
         e.preventDefault();
+        requestAuthenticationCode({email: PAGE.INPUT_EMAIL.value});
         closeWindows();
-        requestСode({email: PAGE.INPUT_EMAIL.value});
         PAGE.CODE_WINDOW.classList.add('active');
+        PAGE.CONFIRMATION_FORM.addEventListener('submit', (e) => {
+            e.preventDefault();
+            Cookies.set('token', PAGE.CODEfromINPUT.value);
+            closeWindows();
+            PAGE.MAIN_WINDOW.classList.remove('blur');
+        });
     });
 });
 PAGE.MESSAGE_BAR.addEventListener('submit', (e) => {
@@ -28,23 +39,40 @@ PAGE.MESSAGE_BAR.addEventListener('submit', (e) => {
     currentMessage.firstElementChild.innerHTML = PAGE.INPUT_BAR.value;
     currentMessage.lastElementChild.innerHTML = getTime();
     PAGE.CHAT_BOX.append(currentMessage);
-    PAGE.MESSAGE_BODY.scrollTop = PAGE.MESSAGE_BODY.scrollHeight;
+    moveScroll();
     PAGE.INPUT_BAR.value = "";
 });
-async function requestСode(email) {
+async function requestAuthenticationCode(email) {
     try {
         const response = await fetch(URL, {
             method: 'POST',
             body: JSON.stringify(email),
             headers: {
                 'Accept': 'application/json',
-                'Content-Type': 'application/json'
+                'Content-Type': 'application/json',
             },
         });
         const json = await response.json();
         return JSON.stringify(json);
     } catch (error) {
-        return error;
+        console.error(error);
+    }
+}
+async function requestUsername(name, token) {
+    try {
+        const response = await fetch(URL, {
+            method: 'PATCH',
+            body: JSON.stringify({'name' : name}),
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            },
+        });
+        const json = await response.json();
+        return JSON.stringify(json);
+    } catch (error) {
+        console.error(error);
     }
 }
 function closeWindows() {
@@ -56,4 +84,6 @@ function getTime() {
     let minutes = new Date().getMinutes().lenght == 2 ? new Date().getMinutes() : '0' + new Date().getMinutes();
     return `${new Date().getHours()}:${minutes}`;
 }
-function renderMessage() {}
+function moveScroll() {
+    PAGE.MESSAGE_BODY.scrollTop = PAGE.MESSAGE_BODY.scrollHeight;
+}
--- js/view.js
+++ js/view.js
@@ -8,10 +8,14 @@ export const PAGE = {
     EMAIL_FORM: document.querySelector('.LogInOut__authorizationForm.authorization'),
     INPUT_EMAIL: document.querySelector('.authorization__email'),
     CODE_WINDOW: document.querySelector('.enterCode'),
+    CONFIRMATION_FORM:document.querySelector('.confirmationForm'),
+    CODEfromINPUT: document.querySelector('.confirmationForm__code'),
     TEMP: document.querySelector('#temp'),
     MESSAGE_BAR: document.querySelector('.sendMessage__bar'),
     INPUT_BAR: document.querySelector('.sendMessage__input'),
     CHAT_BOX: document.querySelector('.chatBox'),
-    MESSAGE_BODY: document.querySelector('.chat')
+    MESSAGE_BODY: document.querySelector('.chat'),
+    NAME_FORM: document.querySelector('.nameChoice'),
+    NAME_INPUT: document.querySelector('.nameChoice__input')
 };
 export const URL = 'https://chat1-341409.oa.r.appspot.com/api/user';
