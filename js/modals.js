48d66e5776f2dbfbdde0877387d503db21d6d672
Ssaveliy <saveliy-sokolnikov@mail.ru>
2022-03-04 10:34:02 +0300

    the structure of the function that contains the logic for displaying modal windows has been changed to improve readability

--- js/modals.js
+++ js/modals.js
@@ -1,44 +1,50 @@
 import {PAGE} from "./view.js";
 import {requestAuthenticationCode} from "./api.js";
 import Cookies from 'js-cookie';
+
+export function closeCurrentWindow(elem) {
+    elem.currentTarget.parentElement.classList.remove('active');
+    PAGE.MAIN_WINDOW.classList.remove('blur');
+}
+
 export function modalWindowCallLogic() {
-    PAGE.EMAIL_FORM.addEventListener('submit', (e) => {
-        e.preventDefault();
+    PAGE.EMAIL_FORM.addEventListener('submit', sendAuthenticationCode);
 
-        if (!PAGE.INPUT_EMAIL.value) return;
-
-        requestAuthenticationCode({email: PAGE.INPUT_EMAIL.value});
-        closeAllWindows();
-
-        let pathSubmit = PAGE.EMAIL_FORM.getAttribute('data-path');
-        PAGE.MAIN_WINDOW.classList.add('blur');
-        document.querySelector(`[data-target = ${pathSubmit}]`).classList.add('active');
-    });
-    
     PAGE.BTN.forEach((event) => {
-        event.addEventListener('click', (e) => {
-            if (e.currentTarget == PAGE.LOG_BTN && e.currentTarget.innerHTML == 'Выйти'){
-                console.log('da');
-                Cookies.remove('token');
-                location.reload();
-            }
-            closeAllWindows();
-
-            let pathBtn = e.currentTarget.getAttribute('data-path');
-            PAGE.MAIN_WINDOW.classList.add('blur');
-            document.querySelector(`[data-target = ${pathBtn}]`).classList.add('active');
-        });
+        event.addEventListener('click', clickOnBtn);
     });
     PAGE.EXIT_BTN.forEach((event) => {
         event.addEventListener('click', closeCurrentWindow);
     });
 }
+
+
+function clickOnBtn(e){
+    if (e.currentTarget == PAGE.LOG_BTN && e.currentTarget.innerHTML == 'Выйти'){
+        console.log('da');
+        Cookies.remove('token');
+        location.reload();
+    }
+    closeAllWindows();
+
+    let pathBtn = e.currentTarget.getAttribute('data-path');
+    PAGE.MAIN_WINDOW.classList.add('blur');
+    document.querySelector(`[data-target = ${pathBtn}]`).classList.add('active');
+}
+function sendAuthenticationCode(e) {
+    e.preventDefault();
+
+    if (!PAGE.INPUT_EMAIL.value) return;
+
+    requestAuthenticationCode({email: PAGE.INPUT_EMAIL.value});
+    closeAllWindows();
+
+    let pathSubmit = PAGE.EMAIL_FORM.getAttribute('data-path');
+    PAGE.MAIN_WINDOW.classList.add('blur');
+    document.querySelector(`[data-target = ${pathSubmit}]`).classList.add('active');
+}
 function closeAllWindows() {
     PAGE.MODAl_WINDOWS.forEach((e) => {
         e.classList.remove('active');
     });
-}
-export function closeCurrentWindow(elem) {
-    elem.currentTarget.parentElement.classList.remove('active');
-    PAGE.MAIN_WINDOW.classList.remove('blur');
 }
